3028955da540798840a58fea754b0c94fcb02ab8
Mark Gamboa <150511627+MarkGamboaa@users.noreply.github.com>
2024-11-15 19:05:52 +0800

    Update Current.java

--- Current.java
+++ Current.java
@@ -5,6 +5,12 @@ public class Current extends accounts {
 
     @Override
     public boolean withdraw(int amount){
+        while(amount < 500){ // Minimum withdrawal amount is 500
+            System.out.println("Withdrawal amount must be at least PHP 500.");
+            System.out.print("Enter a valid withdrawal amount: ");
+            amount = input.nextInt();  // Prompt user for a new amount
+        }
+        
         if(this.getBalance() - amount < 0){
             System.out.println("Insufficient funds.");
             return false;
@@ -18,4 +24,17 @@ public class Current extends accounts {
             return true;
         }
     }
+    
+    @Override
+    public void deposit(int amount){
+        while(amount < 500){ // Minimum deposit amount is 500
+            System.out.println("Deposit amount must be at least PHP 500.");
+            System.out.print("Enter a valid deposit amount: ");
+            amount = input.nextInt();  // Prompt user for a new amount
+        }
+        
+        this.setBalance(this.getBalance() + amount);
+        System.out.println("Deposit successful! New balance: PHP " + this.getBalance());
+    }
+    
 }
